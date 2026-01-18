76aba567709cdea2caae16748cac988d8848d1ae
Pratham Suri <iprathamsuri@gmail.com>
2026-01-18 22:29:49 +0530

    Day-2

---
+++ L2-FirstNodeServer/app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+// function requestListener(req, res){
+//   console.log(req);
+// }
+// http.createServer(requestListener);
+
+const server = http.createServer((req,res)=>{
+  console.log(req);
+  
+}); 
+
+const PORT= 3001;
+server.listen(PORT,()=>{
+  console.log(`Server running port http://localhost:${PORT}`);
+});
